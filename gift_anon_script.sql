e929e1b8ebfb9b3e6318de928ad0f29ac17f2249
Jarrod Vickers <129568970+dev-iant@users.noreply.github.com>
2023-11-10 13:17:06 -0600

    added sql db

---
+++ gift_anon_script.sql
@@ -0,0 +1,58 @@
+-- MySQL Workbench Forward Engineering
+
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
+
+-- -----------------------------------------------------
+-- Schema giftAnon
+-- -----------------------------------------------------
+DROP SCHEMA IF EXISTS `giftAnon` ;
+
+-- -----------------------------------------------------
+-- Schema giftAnon
+-- -----------------------------------------------------
+CREATE SCHEMA IF NOT EXISTS `giftAnon` DEFAULT CHARACTER SET utf8 ;
+USE `giftAnon` ;
+
+-- -----------------------------------------------------
+-- Table `giftAnon`.`users`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `giftAnon`.`users` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `first_name` VARCHAR(45) NOT NULL,
+  `last_name` VARCHAR(45) NOT NULL,
+  `email` VARCHAR(100) NOT NULL,
+  `password` VARCHAR(150) NOT NULL,
+  `created_at` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `giftAnon`.`purchases`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `giftAnon`.`purchases` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `item_name` VARCHAR(100) NOT NULL,
+  `category` VARCHAR(45) NOT NULL,
+  `facility` VARCHAR(75) NOT NULL,
+  `city` VARCHAR(75) NOT NULL,
+  `quantity` INT NOT NULL,
+  `created_at` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `user_id` INT NOT NULL,
+  PRIMARY KEY (`id`),
+  INDEX `fk_purchases_users_idx` (`user_id` ASC) VISIBLE,
+  CONSTRAINT `fk_purchases_users`
+    FOREIGN KEY (`user_id`)
+    REFERENCES `giftAnon`.`users` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
